2c88e7cc7bf5b284c01feeeab3b77aa0c551bc95
Manuel Bilbao <bilbaomanuel98@gmail.com>
2023-03-28 13:27:39 -0300

    Agregados mensajes para finalizar y consultar ganadores. Modificado protocolo (cliente)

--- client/common/client.py
+++ client/common/client.py
@@ -69,20 +69,9 @@ class Client:
         finally:
             file.close()
 
-    # Try to send bets to server. Return True on success
-    def send_bets(self, bets: list) -> bool:
+    def send_message(self, message: dict) -> bool:
         try:
-            msg = {
-                "agency": self.config.id,
-                "bets": bets
-            }
-            encoded_msg = json.dumps(msg).encode("utf-8")
-
-            logging.info(f"Sending {len(encoded_msg)} bytes")
-            if len(encoded_msg) > 8190:
-                self.config.chunk_size = int(self.config.chunk_size * 0.95)
-                return False
-
+            encoded_msg = json.dumps(message).encode("utf-8")
             encoded_size = len(encoded_msg).to_bytes(2, "little",
                                                      signed=False)
             self.conn.sendall(encoded_size + encoded_msg)
@@ -94,6 +83,15 @@ class Client:
                 f'client_id: {self.config.id} | error: {e}'
             )
             return False
+
+    # Try to send bets to server. Return True on success
+    def send_bets(self, bets: list) -> bool:
+        msg = {
+            "agency": self.config.id,
+            "action": "bets",
+            "bets": bets
+        }
+        return self.send_message(msg)
 
     def read_response(self, bets_sent: int, retries: int) -> (int, int):
         try:
@@ -152,6 +150,52 @@ class Client:
 
         return bets_sent, retries
 
+    def send_finish(self):
+        self.create_client_socket()
+
+        msg = {
+            "agency": self.config.id,
+            "action": "finish"
+        }
+        self.send_message(msg)
+
+        self.conn.close()
+
+    def ask_winners(self):
+        self.create_client_socket()
+
+        msg = {
+            "agency": self.config.id,
+            "action": "winners"
+        }
+        self.send_message(msg)
+
+        try:
+            msg = self.conn.recv(2)
+            length = int.from_bytes(msg, "little", signed=False)
+            if length > 8190:
+                raise Exception("Message exceeded maximum length")
+
+            msg = ""
+            while len(msg) < length-1:
+                msg += self.conn.recv(length).rstrip().decode("utf-8")
+            data = json.loads(msg)
+
+            if not data["success"]:
+                raise Exception(data["error"])
+
+            logging.info(
+                'action: consulta_ganadores | result: success | '
+                f'cant_ganadores: {len(data["winners"])}'
+            )
+        except Exception as e:
+            logging.error(
+                f'action: consulta_ganadores | result: fail | error: {e}'
+            )
+
+        finally:
+            self.conn.close()
+
     # start_client_loop Send messages to the client until
     # some time threshold is met
     def start_client_loop(self):
@@ -159,7 +203,6 @@ class Client:
 
         bets_sent = 0
         retries = 0
-        logging.info(f"len(bets): {len(bets)}")
         while bets_sent < len(bets) and retries < 3:
             # Create the connection to the server
             self.create_client_socket()
@@ -171,15 +214,17 @@ class Client:
 
             bets_sent, retries = self.read_response(bets_sent, retries)
 
-        if bets_sent == len(bets):
-            logging.info(
-                'action: apuestas_enviadas | result: success | '
-                f'client_id: {self.config.id}'
-            )
-        elif retries == 3:
+        if retries == 3:
             logging.error(
                 'action: apuestas_enviadas | result: fail | '
                 f'client_id: {self.config.id} | '
                 'error: Too many retries on error'
             )
             sys.exit(-1)
+
+        logging.info(
+            'action: apuestas_enviadas | result: success | '
+            f'client_id: {self.config.id}'
+        )
+        self.send_finish()
+        self.ask_winners()
